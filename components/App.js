0bd3b07f8e9e00e0b616729177c52e26bdcf23f3
Гудков Никита Сергеевич <gudkov.nikita@bk.ru>
2020-11-20 18:28:00 +0300

    App

---
+++ components/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {useRoutes} from 'hookrouter';
+import HomePage from "./HomePage";
+import SingIn from "./SingIn";
+import SingUp from "./SingUp";
+import NotFoundPage from "./NotFoundPage";
+import HMenu from "./Menu";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+    margin: 1em 0 0.5em 0;
+    font-weight: 100;
+    text-transform: uppercase;
+    color: #3A62CA;
+    font-style: italic;
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 58px;
+    line-height: 54px;
+    text-shadow: 2px 5px 0 rgba(0,0,0,0.2);
+    text-align: center
+`
+
+export const routes = {
+    '/': () => <HomePage />,
+    '/auth': () => <SingIn />,
+    '/register': () => <SingUp />
+};
+
+export default function App(){
+        const match = useRoutes(routes);
+        return(
+            <div>
+                <HMenu />
+                <H1>GachiNotes</H1>
+                {match || <NotFoundPage />}
+            </div>
+        )
+}
