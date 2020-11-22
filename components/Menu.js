8a57c71552221463bd6bb92c419376316eb1a914
Гудков Никита Сергеевич <gudkov.nikita@bk.ru>
2020-11-23 00:25:37 +0300

    Add styles

--- components/Menu.js
+++ components/Menu.js
@@ -1,38 +1,26 @@
 import React from 'react';
 import {navigate} from 'hookrouter';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import styled from 'styled-components';
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
+const DivFlex = styled.div`
+    display: flex;
+`
 
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box p={3}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
+const DivProf = styled.div`
+    align-self: end;
+`
 
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-};
+const StyledButton = styled(Button)`
+    color: white;
+    align-self: center;
+`
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -44,9 +32,18 @@ const useStyles = makeStyles((theme) => ({
 export default function SimpleTabs() {
     const classes = useStyles();
     const [value, setValue] = React.useState(0);
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
+    };
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
     };
 
     return (
@@ -56,43 +53,21 @@ export default function SimpleTabs() {
                     <Tab label="Home" onClick={() => navigate('/')} />
                     <Tab label="Sign In" onClick={() => navigate('/auth')} />
                     <Tab label="Sign Up" onClick={() => navigate('/register')} />
+                    <StyledButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                        Open Menu
+                    </StyledButton>
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </Menu>
                 </Tabs>
             </AppBar>
         </div>
     );
 }
-
-/*import React from 'react';
-import {A,navigate} from "hookrouter";
-import styled from "styled-components";
-import Button from '@material-ui/core/Button';
-
-const Men = styled.ul`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    align-items: center;
-    background-color: #bbebff;
-`;
-
-const DivMenu = styled.div`
-    background-color: #bbebff;
-`
-
-export default function HMenu () {
-    return(
-        <div>
-            <Men>
-                <div>
-                    <Button variant="contained" size="large" onClick={() => navigate('/')}>Main</Button>
-                </div>
-                <div>
-                    <Button variant="contained" size="large" onClick={() => navigate('/auth')}>Sing In</Button>
-                </div>
-                <div>
-                    <Button variant="contained" size="large" onClick={() => navigate('/register')}>Sing Up</Button>
-                </div>
-            </Men>
-        </div>
-    )
-}*/
