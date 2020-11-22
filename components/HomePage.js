e8820a6305f6b1bc41bc64b4dab66d0675a025db
Гудков Никита Сергеевич <gudkov.nikita@bk.ru>
2020-11-23 00:24:21 +0300

    Add text

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    text-align: center;
+    font-size : 200%;
+    font-family : Arial, Gadget, sans-serif;
+`
 
 export default function HomePage () {
     return(
-        <div>
-            <h2>Main page</h2>
-        </div>
+        <StyledDiv>
+            <h2>Welcome to the club, buddy</h2>
+            <p>On this site you can make you notes</p>
+            <img src='../public/billy.jpeg' alt="Oh fuck you leatherman"/>
+        </StyledDiv>
     )
 }
