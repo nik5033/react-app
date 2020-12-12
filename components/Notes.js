7a82b1564fd3cfcc2e2d2824765f08d9b9cc4e53
Гудков Никита Сергеевич <gudkov.nikita@bk.ru>
2020-12-12 22:36:37 +0300

    Change note

--- components/Notes.js
+++ components/Notes.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 import styled from "styled-components";
+import MuiAccordion from '@material-ui/core/Accordion';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
+import {Add} from "@material-ui/icons";
+import NoteFiled from "./NoteField";
+import Button from "@material-ui/core/Button";
+
+const AddNoteDiv = styled.div`
+      display: flex;
+      flex-direction: row;
+`
+
+const StyledIcon = styled(IconButton)`
+      height: 40px;
+`
 
 const StyledDiv = styled.div`
       display: flex;
@@ -11,37 +25,56 @@ const StyledDiv = styled.div`
       flex-direction: row;
 `
 
-export default function Profile() {
+const StyledHR = styled.hr`
+    color: #3A62CA;
+`
+
+
+export default function Notes() {
+    const [show, setShow] = React.useState(false)
     const [time, setTime] = React.useState(new Date());
 
-    const Time = (event) => {
+    const [expanded, setExpanded] = React.useState('');
+
+    const handleChange = (panel) => (event, newExpanded) => {
+        setExpanded(newExpanded ? panel : false);
+    };
+
+    const Time = () => {
         setTime(new Date())
     }
 
+    const handleClick = () => {
+        setShow(!show)
+    }
+
     return (
-        <StyledDiv>
-            <Card variant="outlined">
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        Note1
-                    </Typography>
-                    <Typography variant="body2" component="p">
-                        Context1fghdfgdgdfgfdsgfdgfdgdfsgdf
-                    </Typography>
-                    <p>{time.getDate().toString()}.{time.getMonth().toString()}.{time.getFullYear().toString()} {time.getHours().toString()}:{time.getMinutes().toString()}:{time.getSeconds().toString()}</p>
-                </CardContent>
-            </Card>
-            <Card variant="outlined">
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        Note2
-                    </Typography>
-                    <Typography variant="body2" component="p">
-                        Context2
-                    </Typography>
-                    <p>{time.getDate().toString()}.{time.getMonth().toString()}.{time.getFullYear().toString()} {time.getHours().toString()}:{time.getMinutes().toString()}:{time.getSeconds().toString()}</p>
-                </CardContent>
-            </Card>
+        <div>
+            <form>
+                <AddNoteDiv>
+                    <StyledIcon onClick={handleClick}>
+                        <Add />
+                    </StyledIcon>
+                    {show && (<NoteFiled />)}
+                </AddNoteDiv>
+            </form>
+            <StyledHR/>
+            <StyledDiv>
+                <div>
+                    <MuiAccordion square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                        <MuiAccordionSummary>
+                            <Typography>Collapsible Group Item #1</Typography>
+                        </MuiAccordionSummary>
+                        <MuiAccordionDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </MuiAccordionDetails>
+                    </MuiAccordion>
+                </div>
         </StyledDiv>
+        </div>
     );
 }
