c342a8eaaec0b7c58ab78e20dc99065bc3fd3d4c
Гудков Никита Сергеевич <gudkov.nikita@bk.ru>
2020-12-15 11:46:04 +0300

    Add text fields

--- components/NoteField.js
+++ components/NoteField.js
@@ -2,25 +2,73 @@ import React from 'react';
 import styled from "styled-components";
 import TextField from '@material-ui/core/TextField';
 import Button from "@material-ui/core/Button";
+import {navigate} from "hookrouter";
 
 const StyledDiv = styled.div`
       display: flex;
       justify-content: flex-start;
-      flex-direction: row;
+      flex-direction: column;
 `
 
 const Buttons = styled(Button)`
+    margin-top: 10px;
     height: 40px;
 `
 
-export default function NoteField() {
+const StyledTextField = styled(TextField)`
+  margin-top: 10px;
+`
 
+export default function NoteField() {
+    const [title, setTitle] = React.useState('');
+    const [text, setText] = React.useState('');
+
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+
+    const handleClick = (e) => {
+        const input = JSON.stringify({
+            username: localStorage.getItem('username'),
+            title: title,
+            text: text,
+            data: new Date()
+        })
+
+        fetch('/api/note/add', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: input
+        })
+            .then((resp) => {
+                return resp.json()
+            })
+            .then((data) => {
+                alert(data.message);
+                navigate('/notes')
+            })
+            .catch(e => {console.error(e)})
+        e.preventDefault();
+    }
 
     return (
         <StyledDiv>
-                <TextField label="Title" variant="outlined" size='small'/>
-                <TextField label="Text" variant="outlined" size='small'/>
-                <Buttons variant="outlined" color="primary" >
+                <StyledTextField label="Title" variant="outlined" size='small' onChange={e => {setTitle(e.target.value)}}/>
+                <StyledTextField
+                    label="Multiline"
+                    multiline
+                    rows={4}
+                    variant="outlined"
+                    onChange={handleChange}
+                />
+                <Buttons
+                    variant="outlined"
+                    color="primary"
+                    onClick={handleClick}
+                >
                     Send
                 </Buttons>
         </StyledDiv>
